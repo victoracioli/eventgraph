c44578dd169110f50191b762158c75bcaca12b83
Victor <victoraciolica@gmail.com>
2013-05-13 20:20:24 -0700

    Changing of plans

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Eventgraph::Application.config.secret_token = 'c62c252b368aaeabc439115c48713162b75102b92f9ada739e4481507e6ae34acab87232270b26bc49d681cc347e56e1849e519540c911626fd695803e6faaf5'
